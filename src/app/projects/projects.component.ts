f6301fc6dcfb0e7ef90ebdd57af9cd4c95f9136d
DAI ANDO <daifloss@gmail.com>
2017-07-17 10:09:41 +0900

    fist commit

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,62 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { AnimationEvent } from '@angular/animations';
+
+import { Project } from './project.model';
+
+import { ProjectsService } from './projects.service';
+import {
+  markedTrigger,
+  itemStateTrigger,
+  slideStateTrigger,
+  listStateTrigger
+} from './animations';
+import { routeFadeStateTrigger, routeSlideStateTrigger } from '../shared/route-animations';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css'],
+  animations: [
+    markedTrigger,
+    itemStateTrigger,
+    slideStateTrigger,
+    routeFadeStateTrigger,
+    routeSlideStateTrigger,
+    listStateTrigger
+  ]
+})
+export class ProjectsComponent implements OnInit {
+  // @HostBinding('@routeFadeState') routeAnimation = true;
+  @HostBinding('@routeSlideState') routeAnimation = true;
+  projects: Project[];
+  markedPrjIndex = 0;
+  progress = 'progressing';
+  createNew = false;
+
+  constructor(private prjService: ProjectsService) { }
+
+  ngOnInit() {
+    this.prjService.loadProjects()
+      .subscribe(
+        (prj: Project[]) => {
+          this.progress = 'finished';
+          this.projects = prj;
+        }
+      );
+  }
+
+  onStatusUpdated(newStatus: string, id: number) {
+    this.projects[id].status = newStatus;
+  }
+
+  onProjectDeleted(index: number) {
+    this.projects.splice(index, 1);
+  }
+
+  onProjectCreated(project: Project) {
+    this.createNew = false;
+    setTimeout(() => {
+      this.projects.unshift(project);
+    }, 300);
+  }
+}
